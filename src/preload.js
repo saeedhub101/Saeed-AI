348a4a70f076e8653ffca33f33b75aa9ee2de5fd
saeedhub101 <saeid_w@yahoo.com>
2026-09-21 01:39:50 +0300

    Add native desktop movement and UI events

--- src/preload.js
+++ src/preload.js
@@ -4,6 +4,10 @@ contextBridge.exposeInMainWorld("saeed",{
  capture:()=>ipcRenderer.invoke("capture"),
  getSettings:()=>ipcRenderer.invoke("settings:get"),
  setSettings:s=>ipcRenderer.invoke("settings:set",s),
+ moveWindowBy:(dx,dy)=>ipcRenderer.send("window:move-by",dx,dy),
+ showChat:()=>ipcRenderer.send("window:show-chat"),
  onEvent:f=>ipcRenderer.on("agent:event",(_,e)=>f(e)),
- onScreenCapture:f=>ipcRenderer.on("screen:capture",(_,e)=>f(e))
+ onScreenCapture:f=>ipcRenderer.on("screen:capture",(_,e)=>f(e)),
+ onShowChat:f=>ipcRenderer.on("chat:show",()=>f()),
+ onShowSettings:f=>ipcRenderer.on("settings:show",()=>f())
 });
