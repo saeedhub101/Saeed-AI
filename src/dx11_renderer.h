7e1c95c6d9619dd1a01175b6b928bb8471325b66
saeedhub101 <saeid_w@yahoo.com>
2026-09-23 11:10:40 +0300

    Use DirectComposition for transparent DirectX avatar surface

--- src/dx11_renderer.h
+++ src/dx11_renderer.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <windows.h>
 #include <d3d11.h>
-#include <dxgi.h>
+#include <dxgi1_2.h>
+#include <dcomp.h>
 #include <wrl.h>
 #include <string>
 #include "dx11_avatar_renderer.h"
@@ -22,17 +23,20 @@ public:
     bool LoadAvatar(const std::wstring& path);
     void ClearAvatar();
     bool HasAvatar() const { return m_avatar.HasAvatar(); }
-
     bool IsInitialized() const { return m_device != nullptr && m_swapChain != nullptr; }
 
 private:
     bool CreateDeviceAndSwapChain();
+    bool CreateCompositionTarget();
     bool CreateRenderTarget();
 
     HWND m_hwnd = nullptr;
     Microsoft::WRL::ComPtr<ID3D11Device> m_device;
     Microsoft::WRL::ComPtr<ID3D11DeviceContext> m_context;
-    Microsoft::WRL::ComPtr<IDXGISwapChain> m_swapChain;
+    Microsoft::WRL::ComPtr<IDXGISwapChain1> m_swapChain;
     Microsoft::WRL::ComPtr<ID3D11RenderTargetView> m_renderTarget;
+    Microsoft::WRL::ComPtr<IDCompositionDevice> m_dcompDevice;
+    Microsoft::WRL::ComPtr<IDCompositionTarget> m_dcompTarget;
+    Microsoft::WRL::ComPtr<IDCompositionVisual> m_dcompVisual;
     SaeedDx11AvatarRenderer m_avatar;
 };
