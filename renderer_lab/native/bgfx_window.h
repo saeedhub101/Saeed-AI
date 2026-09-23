68e27cff593fce4c4ee68b7e48b1534b37ab2620
saeedhub101 <saeid_w@yahoo.com>
2026-09-24 00:27:46 +0300

    Add native renderer lab backend foundation

---
+++ renderer_lab/native/bgfx_window.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "renderer_lab_window.h"
+class BgfxWindow final: public IRenderLabWindow{
+ HWND hwnd_{};
+public:
+ bool Create(HINSTANCE,const wchar_t*,const wchar_t*,int,int,int,int,const wchar_t*) override;
+ void Frame() override;
+};
