6c8874afb996e8cc6cf80fb9137fe8ac9d54b42b
saeedhub101 <saeid_w@yahoo.com>
2026-09-24 14:33:19 +0300

    feat: expose notifications, updates and microphone controls through secure preload

--- src/preload.js
+++ src/preload.js
@@ -3,14 +3,19 @@ contextBridge.exposeInMainWorld("saeed",{
  chat:(text,image=null)=>ipcRenderer.invoke("chat",{text,image}),
  capture:()=>ipcRenderer.invoke("capture"),
  getHistory:()=>ipcRenderer.invoke("history:get"),
- getSettings:()=>ipcRenderer.invoke("settings:get"),
- setSettings:s=>ipcRenderer.invoke("settings:set",s),
+ checkForUpdates:()=>ipcRenderer.invoke("updates:check"),
+ getNotifications:()=>ipcRenderer.invoke("notifications:get"),
+ clearNotifications:()=>ipcRenderer.invoke("notifications:clear"),
  moveWindowBy:(dx,dy)=>ipcRenderer.send("window:move-by",dx,dy),
- showChat:()=>ipcRenderer.send("window:show-chat"),hideChat:()=>ipcRenderer.send("window:hide-chat"),setIgnoreMouseEvents:ignore=>ipcRenderer.send("window:set-ignore-mouse-events",Boolean(ignore)),
+ showChat:()=>ipcRenderer.send("window:show-chat"),hideChat:()=>ipcRenderer.send("window:hide-chat"),
+ setIgnoreMouseEvents:ignore=>ipcRenderer.send("window:set-ignore-mouse-events",Boolean(ignore)),
  onEvent:f=>ipcRenderer.on("agent:event",(_,e)=>f(e)),
  onConfirmation:f=>ipcRenderer.on("agent:confirm",(_,e)=>f(e)),
  respondConfirmation:(id,approved)=>ipcRenderer.invoke("agent:confirm-response",id,approved),
  onScreenCapture:f=>ipcRenderer.on("screen:capture",(_,e)=>f(e)),
  onShowChat:f=>ipcRenderer.on("chat:show",()=>f()),
- onShowSettings:f=>ipcRenderer.on("settings:show",()=>f())
+ onNotification:f=>ipcRenderer.on("notification:new",(_,e)=>f(e)),
+ onNotificationList:f=>ipcRenderer.on("notifications:list",(_,e)=>f(e)),
+ onMute:f=>ipcRenderer.on("voice:mute",(_,e)=>f(Boolean(e))),
+ onMic:f=>ipcRenderer.on("voice:mic",(_,e)=>f(Boolean(e)))
 });
