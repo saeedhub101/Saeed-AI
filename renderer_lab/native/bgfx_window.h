becd9be534e15ec01bcb9e13a03490d49ab105ac
saeedhub101 <saeid_w@yahoo.com>
2026-09-24 13:58:56 +0300

    Remove retired bgfx lab header

--- renderer_lab/native/bgfx_window.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "renderer_lab_window.h"
-class BgfxWindow final: public IRenderLabWindow{
- HWND hwnd_{};
-public:
- bool Create(HINSTANCE,const wchar_t*,const wchar_t*,int,int,int,int,const wchar_t*) override;
- void Frame() override;
-};
