58312ddaa5731f2c69d33cd69969b5ca50f9a2ad
saeedhub101 <saeid_w@yahoo.com>
2026-09-24 13:59:50 +0300

    Remove retired renderer lab interface

--- renderer_lab/native/renderer_lab_window.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <windows.h>
-class IRenderLabWindow{
-public:
- virtual ~IRenderLabWindow()=default;
- virtual bool Create(HINSTANCE,const wchar_t*,const wchar_t*,int,int,int,int,const wchar_t*)=0;
- virtual void Frame()=0;
-};
