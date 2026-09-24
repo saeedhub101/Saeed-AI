d49b53ab9f1a1eb867cedffa9f65481b3c2a1b64
saeedhub101 <saeid_w@yahoo.com>
2026-09-24 13:55:22 +0300

    Add desktop click-through controls

--- src/preload.js
+++ src/preload.js
@@ -6,7 +6,7 @@ contextBridge.exposeInMainWorld("saeed",{
  getSettings:()=>ipcRenderer.invoke("settings:get"),
  setSettings:s=>ipcRenderer.invoke("settings:set",s),
  moveWindowBy:(dx,dy)=>ipcRenderer.send("window:move-by",dx,dy),
- showChat:()=>ipcRenderer.send("window:show-chat"),
+ showChat:()=>ipcRenderer.send("window:show-chat"),hideChat:()=>ipcRenderer.send("window:hide-chat"),setIgnoreMouseEvents:ignore=>ipcRenderer.send("window:set-ignore-mouse-events",Boolean(ignore)),
  onEvent:f=>ipcRenderer.on("agent:event",(_,e)=>f(e)),
  onConfirmation:f=>ipcRenderer.on("agent:confirm",(_,e)=>f(e)),
  respondConfirmation:(id,approved)=>ipcRenderer.invoke("agent:confirm-response",id,approved),
