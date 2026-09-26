a9a213302888d7a284604222c517c9fadc560b9d
saeedhub101 <saeid_w@yahoo.com>
2026-09-27 00:08:22 +0300

    Expose permissions settings API

--- src/preload.js
+++ src/preload.js
@@ -4,6 +4,8 @@ contextBridge.exposeInMainWorld("saeed",{
  capture:()=>ipcRenderer.invoke("capture"),
  getHistory:()=>ipcRenderer.invoke("history:get"),
  getSettings:()=>ipcRenderer.invoke("settings:get"),
+ getPermissions:()=>ipcRenderer.invoke("permissions:get"),
+ setPermissions:p=>ipcRenderer.invoke("permissions:set",p),
  setSettings:s=>ipcRenderer.invoke("settings:set",s),
  moveWindowBy:(dx,dy)=>ipcRenderer.send("window:move-by",dx,dy),
  showChat:()=>ipcRenderer.send("window:show-chat"),
